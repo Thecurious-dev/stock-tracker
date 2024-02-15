a255e0226db625d34072fb14f92c9dd0a61c9e59
ogunyomade yemi <yhemyogunyomade@gmail.com>
2024-02-15 07:58:33 +0100

    created admin roles, company creation logic, setting owner id to company id

--- app/Admin/Controllers/CompanyController.php
+++ app/Admin/Controllers/CompanyController.php
@@ -3,10 +3,13 @@
 namespace App\Admin\Controllers;
 
 use App\Models\Company;
+use App\Models\User;
 use Encore\Admin\Controllers\AdminController;
 use Encore\Admin\Form;
 use Encore\Admin\Grid;
+use Encore\Admin\Grid\Filter\Where;
 use Encore\Admin\Show;
+use Illuminate\Support\Facades\DB;
 
 class CompanyController extends AdminController
 {
@@ -24,33 +27,52 @@ class CompanyController extends AdminController
      */
     protected function grid()
     {
+        
         $grid = new Grid(new Company());
+        $grid->disableBatchActions();
+        $grid->quickSearch('name', 'phone_number', 'phone_number_2', 'email');
 
-        $grid->column('id', __('Id'));
-        $grid->column('owner_id', __('Owner id'));
-        $grid->column('name', __('Name'));
+        $grid->column('id', __('ID'))->hide();
+        $grid->column('created_at', __('Registered'))
+        ->display(function ($created_at){
+            return date('Y-m-d', strtotime($created_at));
+        })->sortable();
+
+        $grid->column('updated_at', __('Updated at'))
+        ->display(function ($updated_at){
+            return date('Y-m-d', strtotime($updated_at));
+        })->sortable(); 
+
+        $grid->column('owner_id', __('Owner'))->display(function ($owner_id){
+            $user = User::find($owner_id);
+            if ($user == null){
+                return 'Not found';
+            }
+            return $user->name;
+        })->sortable();
+        $grid->column('name', __('Company Name'));
         $grid->column('email', __('Email'));
         $grid->column('logo', __('Logo'));
-        $grid->column('website', __('Website'));
-        $grid->column('about', __('About'));
-        $grid->column('status', __('Status'));
-        $grid->column('address', __('Address'));
-        $grid->column('license_expire', __('License expire'));
-        $grid->column('phone_number', __('Phone number'));
-        $grid->column('phone_number_2', __('Phone number 2'));
-        $grid->column('pobox', __('Pobox'));
-        $grid->column('color', __('Color'));
-        $grid->column('slogan', __('Slogan'));
-        $grid->column('facebook', __('Facebook'));
-        $grid->column('twitter', __('Twitter'));
-        $grid->column('currency', __('Currency'));
-        $grid->column('settings_worker_can_create_stock_item', __('Settings worker can create stock item'));
-        $grid->column('settings_worker_can_create_stock_record', __('Settings worker can create stock record'));
-        $grid->column('settings_worker_can_create_stock_category', __('Settings worker can create stock category'));
-        $grid->column('settings_worker_can_view_balance', __('Settings worker can view balance'));
-        $grid->column('settings_worker_can_view_stats', __('Settings worker can view stats'));
-        $grid->column('created_at', __('Created at'));
-        $grid->column('updated_at', __('Updated at'));
+        $grid->column('website', __('Website'))->hide();
+        $grid->column('about', __('About'))->hide();
+        $grid->column('status', __('Status'))->display(function ($status){
+            return $status == 'Active' ? 'Active': 'Inactive';
+        });
+
+        $grid->column('address', __('Address'))->hide();
+
+        $grid->column('license_expire', __('License expire'))
+            ->display(function ($license_expire){
+                return date ('Y-m-d', strtotime($license_expire));
+            });
+        $grid->column('phone_number', __('Phone number'))->hide();
+        $grid->column('phone_number_2', __('Phone number 2'))->hide();
+        $grid->column('pobox', __('Pobox'))->hide();
+        $grid->column('color', __('Color'))->hide();
+        $grid->column('slogan', __('Slogan'))->hide();
+        $grid->column('facebook', __('Facebook'))->hide();
+        $grid->column('twitter', __('Twitter'))->hide();
+        
 
         return $grid;
     }
@@ -69,6 +91,7 @@ class CompanyController extends AdminController
         $show->field('owner_id', __('Owner id'));
         $show->field('name', __('Name'));
         $show->field('email', __('Email'));
+        
         $show->field('logo', __('Logo'));
         $show->field('website', __('Website'));
         $show->field('about', __('About'));
@@ -82,6 +105,7 @@ class CompanyController extends AdminController
         $show->field('slogan', __('Slogan'));
         $show->field('facebook', __('Facebook'));
         $show->field('twitter', __('Twitter'));
+        
         $show->field('currency', __('Currency'));
         $show->field('settings_worker_can_create_stock_item', __('Settings worker can create stock item'));
         $show->field('settings_worker_can_create_stock_record', __('Settings worker can create stock record'));
@@ -103,29 +127,75 @@ class CompanyController extends AdminController
     {
         $form = new Form(new Company());
 
-        $form->number('owner_id', __('Owner id'));
-        $form->textarea('name', __('Name'));
-        $form->textarea('email', __('Email'));
-        $form->textarea('logo', __('Logo'));
-        $form->textarea('website', __('Website'));
-        $form->textarea('about', __('About'));
-        $form->text('status', __('Status'));
-        $form->textarea('address', __('Address'));
-        $form->date('license_expire', __('License expire'))->default(date('Y-m-d'));
-        $form->textarea('phone_number', __('Phone number'));
-        $form->textarea('phone_number_2', __('Phone number 2'));
-        $form->textarea('pobox', __('Pobox'));
-        $form->textarea('color', __('Color'));
-        $form->textarea('slogan', __('Slogan'));
-        $form->textarea('facebook', __('Facebook'));
-        $form->textarea('twitter', __('Twitter'));
-        $form->text('currency', __('Currency'))->default('USD');
-        $form->text('settings_worker_can_create_stock_item', __('Settings worker can create stock item'))->default('Yes');
-        $form->text('settings_worker_can_create_stock_record', __('Settings worker can create stock record'))->default('Yes');
-        $form->text('settings_worker_can_create_stock_category', __('Settings worker can create stock category'))->default('Yes');
-        $form->text('settings_worker_can_view_balance', __('Settings worker can view balance'))->default('Yes');
-        $form->text('settings_worker_can_view_stats', __('Settings worker can view stats'))->default('Yes');
+        $admin_role_users = DB::table('admin_role_users')
+                ->Where(['role_id' => 2])->get();
 
+            $company_admins = [];
+                foreach($admin_role_users as $key=>$value){
+                    $user = User::find($value->user_id);
+                    if($user == null){
+                        continue;
+                    }
+                    $company_admins[$user->id]=$user->name; 
+                }
+
+        $form->select('owner_id', __('Company Owner'))->options($company_admins)->required();
+        $form->textarea('name', __('Company Name'));
+        $form->email('email', __('Email'));
+        $form->image('logo', __('Logo'));
+        $form->text('website', __('Website'));
+        $form->text('about', __('About Company'));
+        $form->text('status', __('Status'));
+        $form->text('address', __('Address'));
+        $form->date('license_expire', __('License expire'))->default(date('Y-m-d'));
+        $form->text('phone_number', __('Phone number'));
+        $form->text('phone_number_2', __('Phone number 2'));
+        $form->text('pobox', __('Pobox'));
+        $form->color('color', __('Color'));
+        $form->text('slogan', __('Slogan'));
+        $form->text('facebook', __('Facebook'));
+        $form->text('twitter', __('Twitter'));
+        $form->divider('Settings');
+        $form->text('currency', __('Currency'))->default('USD')->rules('required');
+        $form->radio('settings_worker_can_create_stock_item', __('Can Company work create stock item'))
+            ->options([
+                'Yes' => 'Yes',
+                'No' => 'No'
+            ])
+            ->default('Yes');
+        $form->radio('settings_worker_can_create_stock_record', __('Can Company worker create stock record?'))
+            ->options([
+            'Yes' => 'Yes',
+            'No' => 'No'
+            ])
+            ->default('Yes');
+        $form->radio('settings_worker_can_create_stock_category', __('Can Company worker create stock category?'))
+            ->options([
+            'Yes' => 'Yes',
+            'No' => 'No'
+            ])
+            ->default('Yes');
+        $form->radio('settings_worker_can_view_balance', __('Can Company worker view balance?'))
+            ->options([
+            'Yes' => 'Yes',
+            'No' => 'No'
+             ])
+            ->default('Yes');
+        $form->radio('settings_worker_can_view_stats', __('Can Company worker view stats'))
+            ->options([
+            'Yes' => 'Yes',
+            'No' => 'No'
+            ])
+            ->default('Yes');
+
+        $form->tools(function (Form\Tools $tools){
+            $tools->disableDelete();
+            $tools->disableView();
+        });
+        $form->disableCreatingCheck();
+        $form->disableEditingCheck();
+        $form->disableReset();
+        $form->disableViewCheck();
         return $form;
     }
 }
--- app/Models/Company.php
+++ app/Models/Company.php
@@ -5,7 +5,35 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+use function PHPUnit\Framework\throwException;
+
 class Company extends Model
 {
     use HasFactory;
+
+    protected static function boot(){
+        parent::boot();
+
+        //created hook
+
+        static::created(function($company){
+            $owner = User::find($company->owner_id);
+            if($owner == null){
+                throw new \Exception("Owner not found");
+            }
+            $owner->company_id = $company->id;
+            $owner->save();
+        });
+
+        static::updated(function($company){
+            $owner = User::find($company->owner_id);
+            if($owner == null){
+                throw new \Exception("Owner not found");
+            }
+            $owner->company_id = $company->id;
+            $owner->save();
+
+        });
+
+    }
 }
